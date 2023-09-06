8e9f5e3cb25f983e1f7d83fa61585563a2056953
Zack <zack@theradisic.com>
2023-09-07 01:56:37 +0300

    Improve array

--- src/array.h
+++ src/array.h
@@ -5,22 +5,118 @@ ASSUME_NONNULL_BEGIN
 
 #ifndef ARRAY_H_
 
-void *reallocate(void *pointer, usize old_size, usize new_size);
+typedef struct {
+  u8 *maybe_null ptr;
+  usize len;
+  usize cap;
+} array_t;
 
-#define ALLOCATE(type, count)                                                  \
-  (type *)reallocate(NULL, 0, sizeof(type) * (count))
+#define array_type(T)                                                          \
+  struct {                                                                     \
+    T *maybe_null ptr;                                                         \
+    usize len;                                                                 \
+    usize cap;                                                                 \
+  }
 
-#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+void _array_init(array_t *arr, usize cap, usize elem_size) {
+  usize actual_cap = cap < 4 ? 4 : cap;
+  u8 *ptr = (u8 *)malloc(elem_size * actual_cap);
+  arr->ptr = ptr;
+  arr->cap = actual_cap;
+  arr->len = 0;
+}
 
-//< Strings allocate
+void _array_free(array_t *arr) {
+  free(arr->ptr);
+  arr->ptr = NULL;
+  arr->cap = 0;
+  arr->len = 0;
+}
+
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity)*2)
+#define MIN_X(a, b) ((a) < (b) ? (a) : (b))
 
-#define GROW_ARRAY(type, pointer, old_count, new_count)                        \
-  (type *)reallocate(pointer, sizeof(type) * (old_count),                      \
-                     sizeof(type) * (new_count))
+static bool array_resize(array_t *a, usize elemsize, usize newcap) {
+  assert(newcap >= a->len);
 
-#define FREE_ARRAY(type, pointer, old_count)                                   \
-  reallocate(pointer, sizeof(type) * (old_count), 0)
+  if (a->cap == newcap)
+    return true;
+
+  usize newsize;
+  if (check_mul_overflow((usize)newcap, (usize)elemsize, &newsize))
+    return false;
+
+  a->ptr =
+      // is realloc on NULL fucked?
+      a->ptr == NULL ? ((u8 *)malloc(newsize * elemsize))
+                     : (u8 *)realloc(a->ptr, newsize);
+
+  a->cap = newcap;
+  return true;
+}
+
+bool array_grow(array_t *a, usize elemsize, usize extracap) {
+  usize newcap;
+  if (a->cap == 0) {
+    // initial allocation
+    const usize ideal_nbyte = 64;
+    newcap = MAX(extracap, ideal_nbyte / elemsize);
+  } else {
+    // grow allocation
+    usize currsize = (usize)a->cap * (usize)elemsize;
+    usize extrasize;
+    if (check_mul_overflow((usize)extracap, (usize)elemsize, &extrasize))
+      return false;
+    if (currsize < 65536 && extrasize < 65536 / 2) {
+      // double capacity until we hit 64KiB
+      newcap = (a->cap >= extracap) ? a->cap * 2 : a->cap + extracap;
+    } else {
+      usize addlcap = MAX(65536u / elemsize, CEIL_POW2(extracap));
+      if (check_add_overflow(a->cap, addlcap, &newcap)) {
+        // try adding exactly what is needed (extracap)
+        if (check_add_overflow(a->cap, extracap, &newcap))
+          return false;
+      }
+    }
+  }
+
+  assert(newcap - a->cap >= extracap);
+  return array_resize(a, elemsize, newcap);
+}
+
+void _array_reserve(array_t *arr, usize elem_size, usize amount) {
+  arr->len += amount;
+  if (arr->len > arr->cap) {
+    array_grow(arr, elem_size, arr->len);
+  }
+}
+
+void _array_shrink_to_fit(array_t *arr) {
+  if (arr->cap == arr->len) {
+    return;
+  }
+  arr->ptr = (u8 *)realloc(arr->ptr, arr->len);
+  arr->cap = arr->len;
+}
+
+#define array_empty(T) ((T){.ptr = NULL, .len = 0, .cap = 0})
+#define array_init(T, a, cap) _array_init((array_t *)a, cap, sizeof(T))
+#define array_shrink_to_fit(a) _array_shrink_to_fit((array_t *)a)
+#define array_free(a) _array_free((array_t *)a)
+
+#define array_push(T, a, val)                                                  \
+  ({                                                                           \
+    static_assert(__same_type(T, __typeof__(val)), "");                        \
+    array_t *__a = (array_t *)(a);                                             \
+    (__a->len >= __a->cap && UNLIKELY(!array_grow(__a, sizeof(T), 1)))         \
+        ? false                                                                \
+        : (((T *)__a->ptr)[__a->len++] = (val), true);                         \
+  })
+
+#define array_pop(T, a) (((T *)(a)->ptr)[--a->len])
+#define array_reserve(T, a, n) _array_reserve((array_t *)(a), sizeof(T), n)
+
+void *reallocate(void *pointer, usize old_size, usize new_size);
 
 #define ARRAY_H_
 
--- src/common.h
+++ src/common.h
@@ -17,6 +17,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef long isize;
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -29,6 +35,8 @@ int float_eq(float a, float b);
 #define ZMEM_L1_CACHE_LINE_SIZE 64
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+#define ZERO(a) memset(&a, 0, sizeof(a))
 
 #define IS_POWER_OF_TWO(x) ((x) && !((x) & ((x)-1)))
 /* static inline const int is_power_of_two(int x) { */
@@ -62,8 +70,8 @@ void _panic(const char *file, int line, const char *fun, const char *fmt, ...);
 
 // clang-format off
 #if defined(__clang__) && __has_feature(nullability)
-  #ifndef nullable
-    #define nullable _Nullable
+  #ifndef maybe_null
+    #define maybe_null _Nullable
   #endif
   #define ASSUME_NONNULL_BEGIN                                                \
     _Pragma("clang diagnostic push")                                              \
@@ -74,12 +82,156 @@ void _panic(const char *file, int line, const char *fun, const char *fmt, ...);
     _Pragma("clang diagnostic pop") \
     _Pragma("clang assume_nonnull end")
 #else
-  #ifndef nullable
-    #define nullable
+  #ifndef maybe_null
+    #define maybe_null
   #endif
   #define ASSUME_NONNULL_BEGIN
   #define ASSUME_NONNULL_END
 #endif
 // clang-format on
+
+#define __same_type(a, b)                                                      \
+  __builtin_types_compatible_p(__typeof__(a), __typeof__(b))
+
+// UNLIKELY(integralexpr)->bool
+#if __has_builtin(__builtin_expect)
+#define LIKELY(x) (__builtin_expect((bool)(x), true))
+#define UNLIKELY(x) (__builtin_expect((bool)(x), false))
+#else
+#define LIKELY(x) (x)
+#define UNLIKELY(x) (x)
+#endif
+
+// overflow checking
+
+static inline bool __must_check_unlikely(bool unlikely) {
+  return UNLIKELY(unlikely);
+}
+
+#define check_add_overflow(a, b, dst)                                          \
+  __must_check_unlikely(({                                                     \
+    __typeof__(a) a__ = (a);                                                   \
+    __typeof__(b) b__ = (b);                                                   \
+    __typeof__(dst) dst__ = (dst);                                             \
+    (void)(&a__ == &b__);                                                      \
+    (void)(&a__ == dst__);                                                     \
+    __builtin_add_overflow(a__, b__, dst__);                                   \
+  }))
+
+#define check_sub_overflow(a, b, dst)                                          \
+  __must_check_unlikely(({                                                     \
+    __typeof__(a) a__ = (a);                                                   \
+    __typeof__(b) b__ = (b);                                                   \
+    __typeof__(dst) dst__ = (dst);                                             \
+    (void)(&a__ == &b__);                                                      \
+    (void)(&a__ == dst__);                                                     \
+    __builtin_sub_overflow(a__, b__, dst__);                                   \
+  }))
+
+#define check_mul_overflow(a, b, dst)                                          \
+  __must_check_unlikely(({                                                     \
+    __typeof__(a) a__ = (a);                                                   \
+    __typeof__(b) b__ = (b);                                                   \
+    __typeof__(dst) dst__ = (dst);                                             \
+    (void)(&a__ == &b__);                                                      \
+    (void)(&a__ == dst__);                                                     \
+    __builtin_mul_overflow(a__, b__, dst__);                                   \
+  }))
+
+// bool would_add_overflow(anyint a, anyint b)
+#if __has_builtin(__builtin_add_overflow_p)
+#define would_add_overflow(a, b)                                               \
+  __builtin_add_overflow_p((a), (b), (__typeof__((a) + (b)))0)
+#define would_mul_overflow(a, b)                                               \
+  __builtin_mul_overflow_p((a), (b), (__typeof__((a) * (b)))0)
+#elif __has_builtin(__builtin_add_overflow)
+#define would_add_overflow(a, b)                                               \
+  ({                                                                           \
+    __typeof__((a) + (b)) tmp__;                                               \
+    __builtin_add_overflow((a), (b), &tmp__);                                  \
+  })
+#define would_mul_overflow(a, b)                                               \
+  ({                                                                           \
+    __typeof__((a) * (b)) tmp__;                                               \
+    __builtin_mul_overflow((a), (b), &tmp__);                                  \
+  })
+#else
+// best effort (triggers ubsan if enabled)
+#define would_add_overflow(a, b) ({ \
+    __typeof__((a) + (b)) tmp__ = (i64)(a) + (i64)(b); \
+    (u64)tmp__ < (u64)(a);
+})
+#define would_mul_overflow(a, b) ({ \
+    __typeof__((a) * (b)) tmp__ = (i64)(a) * (i64)(b); \
+    (u64)tmp__ < (u64)(a);
+})
+#endif
+
+#define MIN_X(a, b) ((a) < (b) ? (a) : (b))
+
+// ANYINT CEIL_POW2(ANYINT x) rounds up x to nearest power of two.
+// Returns 1 when x is 0.
+// Returns 0 when x is larger than the max pow2 for x's type (e.g. >0x80000000
+// for u32)
+#define CEIL_POW2(x)                                                           \
+  ({                                                                           \
+    __typeof__(x) xtmp__ = (x);                                                \
+    CEIL_POW2_X(xtmp__);                                                       \
+  })
+// CEIL_POW2_X is a constant-expression implementation of CEIL_POW2
+#define CEIL_POW2_X(x)                                                         \
+  (((x) <= (__typeof__(x))1)                                                   \
+       ? (__typeof__(x))1                                                      \
+       : ((((__typeof__(x))1                                                   \
+            << ILOG2(((x) - ((x) == (__typeof__(x))1)) - (__typeof__(x))1)) -  \
+           (__typeof__(x))1)                                                   \
+          << 1) +                                                              \
+             (__typeof__(x))2)
+
+// int co_clz(ANYUINT x) counts leading zeroes in x,
+// starting at the most significant bit position.
+// If x is 0, the result is undefined.
+#define co_clz(x)                                                              \
+  (_Generic((x), i8                                                            \
+            : __builtin_clz, u8                                                \
+            : __builtin_clz, i16                                               \
+            : __builtin_clz, u16                                               \
+            : __builtin_clz, i32                                               \
+            : __builtin_clz, u32                                               \
+            : __builtin_clz, long                                              \
+            : __builtin_clzl, unsigned long                                    \
+            : __builtin_clzl, long long                                        \
+            : __builtin_clzll, unsigned long long                              \
+            : __builtin_clzll)(x) -                                            \
+   (32 - MIN_X(4, (int)sizeof(__typeof__(x))) * 8))
+
+// int co_ctz(ANYUINT x) counts trailing zeroes in x,
+// starting at the least significant bit position.
+// If x is 0, the result is undefined.
+#define co_ctz(x)                                                              \
+  _Generic((x), i8                                                             \
+           : __builtin_ctz, u8                                                 \
+           : __builtin_ctz, i16                                                \
+           : __builtin_ctz, u16                                                \
+           : __builtin_ctz, i32                                                \
+           : __builtin_ctz, u32                                                \
+           : __builtin_ctz, long                                               \
+           : __builtin_ctzl, unsigned long                                     \
+           : __builtin_ctzl, long long                                         \
+           : __builtin_ctzll, unsigned long long                               \
+           : __builtin_ctzll)(x)
+
+// int co_fls(ANYINT n) finds the Find Last Set bit (last = most-significant)
+// (Note that this is not the same as co_ffs(x)-1).
+// e.g. co_fls(0b1111111111111111) = 15
+// e.g. co_fls(0b1000000000000000) = 15
+// e.g. co_fls(0b1000000000000000) = 15
+// e.g. co_fls(0b1000) = 3
+#define co_fls(x) ((x) ? (int)(sizeof(__typeof__(x)) * 8) - co_clz(x) : 0)
+
+// int ILOG2(ANYINT n) calculates the log of base 2, rounding down.
+// e.g. ILOG2(15) = 3, ILOG2(16) = 4.
+// Result is undefined if n is 0.
+#define ILOG2(n) (co_fls(n) - 1)
 
 #endif // COMMON_H
